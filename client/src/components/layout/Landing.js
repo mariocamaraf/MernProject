3233ada6d4e58e1d6b2997b91305afee83f16777
Mario Camara <mariocamaraf@gmail.com>
2020-07-24 14:11:55 +0200

    Update Landing.js

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,10 +6,10 @@ const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
+          <h1 className="x-large">Dev-Net</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
+            A community between Devs! share your skills, experience, portfolio, posts, and get help from
+            other great developers.
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">Sign Up</Link>
